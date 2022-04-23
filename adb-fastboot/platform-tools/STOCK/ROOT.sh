3682ddc42ae4f44a229611db244915fc47c1e7d8
Timofey Potapov <timofey.potapov@otrs.com>
2022-04-23 22:20:15 +0200

    Updated README and added OS versions.

--- adb-fastboot/platform-tools/STOCK/ROOT.sh
+++ adb-fastboot/platform-tools/STOCK/ROOT.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fastboot flash --disable-verity --disable-verification vbmeta images/vbmeta.img
-fastboot flash --disable-verity --disable-verification vbmeta_system images/vbmeta_system.img
-fastboot flash --slot=all boot images/magisk_patched-*.img
+fastboot flash --disable-verity --disable-verification vbmeta images/latest/vbmeta.img
+fastboot flash --disable-verity --disable-verification vbmeta_system images/latest/vbmeta_system.img
+fastboot flash --slot=all boot images/latest/magisk_patched-*.img
 
--- adb-fastboot/platform-tools/STOCK/UPDATE_FIRMWARE.sh
+++ adb-fastboot/platform-tools/STOCK/UPDATE_FIRMWARE.sh
@@ -3,27 +3,27 @@
 # https://forum.xda-developers.com/t/working-safetynet-with-pixel-6-pro-android-12.4354149/
 #  fastboot --disable-verity --disable-verification update image-raven-xxxxxxxxxxxx.zip --skip-reboot
 
-fastboot flash --slot=all abl images/abl.img
-fastboot flash --slot=all aop images/aop.img
-fastboot flash --slot=all bluetooth images/bluetooth.img
-fastboot flash --slot=all cmnlib64 images/cmnlib64.img
-fastboot flash --slot=all cmnlib images/cmnlib.img
-fastboot flash --slot=all devcfg images/devcfg.img
-fastboot flash --slot=all dsp images/dsp.img
-fastboot flash --slot=all hyp images/hyp.img
-fastboot flash --slot=all imagefv images/imagefv.img
-fastboot flash --slot=all keymaster images/keymaster.img
-fastboot flash --slot=all LOGO images/LOGO.img
-fastboot flash --slot=all modem images/modem.img
-fastboot flash --slot=all multiimgoem images/multiimgoem.img
-fastboot flash --slot=all oem_stanvbk images/oem_stanvbk.img
-fastboot flash --slot=all opproduct images/opproduct.img
-fastboot flash --slot=all qupfw images/qupfw.img
-fastboot flash --slot=all storsec images/storsec.img
-fastboot flash --slot=all tz images/tz.img
-fastboot flash --slot=all uefisecapp images/uefisecapp.img
-fastboot flash --slot=all xbl_config images/xbl_config.img
-fastboot flash --slot=all xbl images/xbl.img
+fastboot flash --slot=all abl images/latest/abl.img
+fastboot flash --slot=all aop images/latest/aop.img
+fastboot flash --slot=all bluetooth images/latest/bluetooth.img
+fastboot flash --slot=all cmnlib64 images/latest/cmnlib64.img
+fastboot flash --slot=all cmnlib images/latest/cmnlib.img
+fastboot flash --slot=all devcfg images/latest/devcfg.img
+fastboot flash --slot=all dsp images/latest/dsp.img
+fastboot flash --slot=all hyp images/latest/hyp.img
+fastboot flash --slot=all imagefv images/latest/imagefv.img
+fastboot flash --slot=all keymaster images/latest/keymaster.img
+fastboot flash --slot=all LOGO images/latest/LOGO.img
+fastboot flash --slot=all modem images/latest/modem.img
+fastboot flash --slot=all multiimgoem images/latest/multiimgoem.img
+fastboot flash --slot=all oem_stanvbk images/latest/oem_stanvbk.img
+fastboot flash --slot=all opproduct images/latest/opproduct.img
+fastboot flash --slot=all qupfw images/latest/qupfw.img
+fastboot flash --slot=all storsec images/latest/storsec.img
+fastboot flash --slot=all tz images/latest/tz.img
+fastboot flash --slot=all uefisecapp images/latest/uefisecapp.img
+fastboot flash --slot=all xbl_config images/latest/xbl_config.img
+fastboot flash --slot=all xbl images/latest/xbl.img
 
 # fastboot reboot-bootloader
 # ping -n 5 127.0.0.1 >nul
